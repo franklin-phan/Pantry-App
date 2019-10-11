e5fe3acdd3d6d16103d42b5d3d557e12072b000a
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2019-10-10 23:31:53 -0700

    Pushing to Heroku

--- app.py
+++ app.py
@@ -6,47 +6,52 @@ import os
 
 app = Flask(__name__)
 
-host = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/my_app_db")
-client = MongoClient(host=f"{host}?retryWrites=false")
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
+
+client = MongoClient(host=f'{host}?retryWrites=false')  
 db = client.get_default_database()
-pantry = db.pantry
-
-@app.route("/")
+pantry = db.playlists
+item = db.item
+cart = db.cart
+#Return pantry
+@app.route("/",methods=['GET'])
 def pantry_index():
-    """Return homepage"""
-    return render_template("pantry_index.html", pantry=pantry.find())
+    """Return Pantry"""
+    
+   
+    
+    return render_template("pantry_index.html", item=list(item.find()))
 
 
-@app.route("/pantry/new")
-def new_item():
-    """Return new listing creation page"""
-    return render_template("pantry_new.html", item={},
-                           title='New item')
 
 
-@app.route("/pantry/new", methods=["POST"])
+#Posting page
+@app.route("/pantry/new", methods=["POST", 'GET'])
 def pantry_new():
-    """Allow the user to create a new listing"""
-    item = {
-        "food name": request.form.get("name"),
-        "image": request.form.get("image"),
-        "price": request.form.get("price"),
-        "description": request.form.get("description")
-    }
-    item_id = pantry.insert_one(pantry).inserted_id
-    return redirect(url_for("pantry_show", item_id=item_id))
+    if request.method == 'POST':
+        item = {
+            "name": request.form['name'],
+            "image": request.form['link'],
+            "price": request.form['price'],
+            "description and quantity": request.form['description']
+            }
+        
+        item_id = pantry.insert_one(item).inserted_id
+        return redirect(url_for("pantry_show", item_id=item_id))
+        # return redirect(url_for("pantry_show", item=item.find()))
+       
+    if request.method == "GET":
+        return render_template("pantry_new.html")
 
-
+#Show single item
 @app.route("/pantry/<item_id>")
 def pantry_show(item_id):
-    """Show a single listing."""
     item = pantry.find_one({"_id": ObjectId(item_id)})
     return render_template("pantry_show.html", item=item)
 
-
-@app.route('/pantrys/<item_id>', methods=['POST'])
+#Submit edited item
+@app.route('/pantry/<item_id>', methods=['POST'])
 def pantry_update(item_id):
-    """Submit an edited listing."""
     item_updated = {
         "food name": request.form.get("name"),
         "image": request.form.get("image"),
@@ -58,20 +63,65 @@ def pantry_update(item_id):
         {'$set': item_updated})
     return redirect(url_for('pantry_show', item_id=item_id))
 
-
+#Show the edit form for an item
 @app.route("/pantry/<item_id>/edit")
 def pantry_edit(item_id):
-    """Show the edit form for a listing."""
     item = pantry.find_one({"_id": ObjectId(item_id)})
     return render_template("pantry_edit.html", item=item,
                            title="Edit listing")
 
-
+#Delete an item
 @app.route('/pantry/<item_id>/delete', methods=['POST'])
 def pantry_delete(item_id):
-    """Delete one listing."""
     pantry.delete_one({'_id': ObjectId(item_id)})
     return redirect(url_for('pantry_index'))
+
+#Shows cart
+@app.route("/pantry/cart")
+def cart_show():
+    return render_template("show_cart.html", cart=cart.find())
+
+#Shows item in user's cart
+@app.route("/pantry/cart/<cart_item_id>")
+def cart_item_show(cart_item_id):
+    cart_item = cart.find_one({"_id": ObjectId(cart_item_id)})
+    return render_template("cart_item_show.html", cart_item=cart_item)
+
+#Delete an item from cart
+@app.route("/pantry/cart/<cart_item_id>/delete", methods=["POST"])
+def cart_delete(cart_item_id):
+    cart.delete_one({"_id": ObjectId(cart_item_id)})
+    return redirect(url_for("show_cart"))
+
+#Delete all items in cart
+@app.route("/pantry/cart/destroy")
+def cart_destroy():
+    for cart_item in cart.find():
+        cart.delete_one({"_id": ObjectId(cart_item["_id"])})
+    return redirect(url_for("show_cart"))
+
+#Cart Checkout
+@app.route("/pantry/cart/checkout")
+def cart_checkout():
+    total = 0
+    for item in cart.find():
+        total += int(item["price"])
+    print("$" + str(total))
+    return redirect(url_for("cart_destroy"))
+
+#Add item to cart
+@app.route('/pantry/<cart_item_id>/add-to-cart', methods=['POST'])
+def add_to_cart(cart_item_id):
+    cart_item = pantry.find_one({"_id": ObjectId(cart_item_id)})
+    for _ in range(int(request.form.get("quant"))):
+        new_item = {
+            "food name": cart_item["name"],
+            "image": cart_item["image"],
+            "price": cart_item["price"],
+            "description and quantity": cart_item["description"]
+            }
+        cart.insert_one(new_item)
+    return redirect(url_for('show_cart'))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tests.py
@@ -0,0 +1,85 @@
+from unittest import TestCase, main as unittest_main, mock
+from app import app
+from bson.objectid import ObjectId
+
+
+sample_item_id = ObjectId('5d55cffc4a3d4031f42827a3')
+sample_item = {
+    'name': 'Spaghetti',
+    'image': 'https://thecozyapron.com/wp-content/uploads/2019/05/spaghetti-bolognese_thecozyapron_1.jpg',
+    'price': '10',
+    "description and quantity":'Good spaghetti and 2 bowls'
+}
+sample_form_data = {
+    'name': sample_item['name'],
+    'image': sample_item['image'],
+    'price': sample_item['price'],
+    'description and quantity': sample_item['description']
+}
+
+
+class listingsTests(TestCase):
+    """Flask tests."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        # Get the Flask test client
+        self.client = app.test_client()
+
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
+
+    def test_index(self):
+        """Test the listings homepage."""
+        result = self.client.get('/')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'listing', result.data)
+
+    def test_new(self):
+        """Test the new listing creation page."""
+        result = self.client.get('/pantry/new')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'New listing', result.data)
+
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_show_listing(self, mock_find):
+        """Test showing a single listing."""
+        mock_find.return_value = sample_item
+
+        result = self.client.get(f'/pantry/{sample_item_id}')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Clown Nose', result.data)
+
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_edit_listing(self, mock_find):
+        """Test editing a single listing."""
+        mock_find.return_value = sample_item
+
+        result = self.client.get(f'/pantry/{sample_item_id}/edit')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Spaghetti', result.data)
+
+    @mock.patch('pymongo.collection.Collection.insert_one')
+    def test_submit_listing(self, mock_insert):
+        """Test submitting a new listing."""
+        result = self.client.post('/pantry/new', data=sample_form_data)
+
+        # After submitting, should redirect to that item's page
+        self.assertEqual(result.status, '302 FOUND')
+        mock_insert.assert_called_with(sample_item)
+
+    @mock.patch('pymongo.collection.Collection.update_one')
+    def test_update_playlist(self, mock_update):
+        result = self.client.post(f'/pantry/{sample_item_id}', data=sample_form_data)
+
+        self.assertEqual(result.status, '302 FOUND')
+        mock_update.assert_called_with({'_id': sample_item_id}, {'$set': sample_item})
+    @mock.patch('pymongo.collection.Collection.delete_one')
+    def test_delete_playlist(self, mock_delete):
+        form_data = {'_method': 'DELETE'}
+        result = self.client.post(f'/pantry/{sample_item_id}/delete', data=form_data)
+        self.assertEqual(result.status, '302 FOUND')
+        mock_delete.assert_called_with({'_id': sample_item_id})
+if __name__ == '__main__':
+    unittest_main()
